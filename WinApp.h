bda39586331b396a6146b93ade6c4fc12f35bac6
PARK_YIJUN <tnswo2445@gmail.com>
2024-07-05 15:02:52 +0900

    03_05

---
+++ WinApp.h
@@ -0,0 +1,36 @@
+#pragma once
+#include <cstdint>
+#include<Windows.h>
+
+class WinApp {
+public:
+
+	//size
+	static const int32_t kClientWidth = 1280;
+	static const int32_t kClientHeight = 720;
+
+	void Initialize();
+
+	void Finalize();
+
+	static LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam);
+
+	HWND GetHwnd() const { return hwnd; }
+
+	HINSTANCE GetHInstance() const { return wc.hInstance; }
+
+	HRESULT GetHRESULT() const { return hr; }
+
+
+	bool ProcessMessage();
+
+private:
+   
+	
+
+	HWND hwnd = nullptr;
+
+	WNDCLASS wc{};
+
+	HRESULT hr;
+};
